7e683c9e77ded3a18fbe87802d002833bc55a12a
Denis 7Jenko Semizhenko <wi-q@yandex.ru>
2025-02-11 03:57:53 +0300

    Добавить запрос вещи. Разбить приложение ShareIt на два — shareIt-server и shareIt-gateway. Добавить тестирование.

--- gateway/src/test/java/ru/practicum/shareIt/controller/BookingControllerTest.java
+++ gateway/src/test/java/ru/practicum/shareIt/controller/BookingControllerTest.java
@@ -3,11 +3,12 @@ package ru.practicum.shareIt.controller;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-import ru.practicum.shareit.ShareItGateway;
+import ru.practicum.shareit.booking.controller.BookingController;
 import ru.practicum.shareit.booking.dto.BookingRequestDto;
 import ru.practicum.shareit.client.BookingClient;
 import static org.mockito.ArgumentMatchers.any;
@@ -16,7 +17,8 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest(classes = ShareItGateway.class)
+@ContextConfiguration(classes = {BookingController.class, BookingClient.class})
+@WebAppConfiguration
 public class BookingControllerTest {
 
     @Autowired
