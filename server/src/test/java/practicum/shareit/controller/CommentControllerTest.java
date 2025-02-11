c8c6ac70b4cc0032dcedc9d99e2291f00f6e6a03
Denis 7Jenko Semizhenko <wi-q@yandex.ru>
2025-02-12 01:29:22 +0300

    Добавить запрос вещи. Разбить приложение ShareIt на два — shareIt-server и shareIt-gateway. Добавить тестирование.

---
+++ server/src/test/java/practicum/shareit/controller/CommentControllerTest.java
@@ -0,0 +1,52 @@
+package practicum.shareit.controller;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.web.servlet.MockMvc;
+import ru.practicum.shareit.ShareItServer;
+import ru.practicum.shareit.comment.dto.CommentDto;
+import ru.practicum.shareit.comment.service.CommentServiceImpl;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(classes = ShareItServer.class)
+@AutoConfigureMockMvc
+@Sql(scripts = {"file:src/main/resources/schema.sql"})
+public class CommentControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private CommentServiceImpl commentClient;
+
+    @Test
+    public void testAddComment() throws Exception {
+        String requestBody = "{\"text\":\"Great item!\"}";
+
+        CommentDto commentDto = new CommentDto();
+        commentDto.setId(1L);
+        commentDto.setText("Great item!");
+
+        Mockito.when(commentClient.addComment(anyLong(), anyLong(), any()))
+                .thenReturn(commentDto);
+
+        mockMvc.perform(post("/items/1/comment")
+                        .header("X-Sharer-User-Id", 1L)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestBody))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(1))
+                .andExpect(jsonPath("$.text").value("Great item!"));
+    }
+}
